270c42344c8645e272b3ed214de258cedc46ae4c
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-07-15 10:58:49 +0100

    successful quine

--- quine.rb
+++ quine.rb
@@ -1,5 +1,9 @@
+# This attempt works by opening itself as a file
+# and reading all of the lines
 def have_a_go
   puts "Will this quine work?"
 end
 
-have_a_go if __FILE__ == $0
+File.open($0, 'r') do |f|
+  f.readlines.each { |line| puts line }
+end
